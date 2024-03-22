ed32e7c93cd328af5513a2bef89a7a5d4af31f4c
DaviAieta <aieta.davi@gmail.com>
2024-03-22 18:00:13 -0300

    Update Interface

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ from expense import Expense
 if __name__ == '__main__':
     control = ControlExpense()
     
+    while True:
+        print('1 - Add expense')
+        print('2 - List expense')
+        print('3 - Exit')
+        
+        option = int(input('Choose the option: '))
+        if(option == 1):
+            description = input('What is the name of the expense?')
+            category = input('What is the expense category?')
+            value = input('What is the value of the expense?')
